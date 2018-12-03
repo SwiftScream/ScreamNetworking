1deef0c453f5475e3662d2e0a2f6c7738f8fe4da
Alex Deem <alexdeem@gmail.com>
2018-12-03 13:17:50 +1100

    Add a little structure to Mac example

---
+++ Example-mac/GitHub.swift
@@ -0,0 +1,68 @@
+//   Copyright 2018 Alex Deem
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+import Foundation
+import ScreamNetworking
+import ScreamEssentials
+
+private let session = GitHubSession()
+private var rootRequest: AutoCancellable?
+private var reposRequest: AutoCancellable?
+private var orgRequest: AutoCancellable?
+
+private func requestRepos(org: GitHubOrganizationResponse) -> AutoCancellable {
+    return session.enqueue(GitHubOrganizationRepos(organization: org)) { response in
+        do {
+            switch try response.unwrap() {
+            case .error(let e):
+                print("Domain Error: \(e.message)")
+            case .success(let repos):
+                print("Repositories: \(repos.count)")
+            }
+        } catch let e {
+            print("Networking Error: \(e)")
+        }
+    }
+}
+
+public func runGitHubExample() {
+    rootRequest = session.enqueue(GitHubRoot()) { response in
+        do {
+            switch try response.unwrap() {
+            case .error(let e):
+                print("Domain Error: \(e.message)")
+            case .success(let root):
+                print("\(root.endpoints.count) Endpoints")
+            }
+        } catch let e {
+            print("Networking Error: \(e)")
+        }
+    }
+
+    orgRequest = session.enqueue(GitHubOrganization(name: "SwiftScream")) { response in
+        do {
+            switch try response.unwrap() {
+            case .error(let e):
+                print("Domain Error: \(e.message)")
+            case .success(let org):
+                print(org.name)
+                print(org.description)
+                print(org.location)
+                reposRequest = requestRepos(org: org)
+            }
+        } catch let e {
+            print("Networking Error: \(e)")
+        }
+    }
+}
